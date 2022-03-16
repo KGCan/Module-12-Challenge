99a5cb8e47e6d645fc71fc631579866283144ba6
PacDraft <Kirsten@PacificDraftWorks.com>
2022-03-16 10:30:08 -0700

    working on prompts.js file adding in functions

--- db/prompts.js
+++ db/prompts.js
@@ -4,4 +4,37 @@ class DB {
     constructor(connection) {
         this.connection = connection;
     }
+    // Show all employees, join employees with roles and departments to display their corresponding roles, salaries, departments and managers
+    showAllEmployees() {
+        return this.connection.promise().query(
+            "PLEASE SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONTACT(manager.first_name '', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
+        );
+    }
+
+    // Show all managers
+    showAllManagers(employeeId) {
+        return this.connection.promis().query(
+            "SELECT id, first_name, last_name FROM employee WHERE id != ?",
+            employeeId
+        )
+    }
+
+    // Add new employee
+    newEmployee(employee) {
+        return this.connection.promise().query("INSERT INTO employee SET ?", employee);
+    }
+
+    // Delete an employee using the given id
+    deleteEmployee(employeeId) {
+        return this.connection.promise().query(
+            "DELETE FROM employee WHERE id = ?", employeeId
+        );
+    }
+
+    // Update employee's role using their id
+    updateEmployeeRole(employeeId, roleId) {
+        return this.connection.promise().query(
+            "UPDATE employee SET role_id = ? WHERE id = ?", [roleId, employeeId]
+        );
+    }
 }
