df8cdef42a4d28b63f30e687c4ff33093948d45b
PacDraft <Kirsten@PacificDraftWorks.com>
2022-03-17 13:04:35 -0700

    corrected connection error in index.js, corrected typo in prompts

--- db/prompts.js
+++ db/prompts.js
@@ -7,7 +7,7 @@ class DB {
     // Show all employees, join employees with roles and departments to display their corresponding roles, salaries, departments and managers
     showAllEmployees() {
         return this.connection.promise().query(
-            "PLEASE SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONTACT(manager.first_name '', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
+            "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name '', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
         );
     }
 
